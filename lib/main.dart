cc284f760ddb41bb653f6f83a16035873ef17c0f
ritikabansal312 <ritikabansal5678@gmail.com>
2025-01-03 02:06:17 +0530

    changes with dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:vehicle_tracker/views/vehicle_screen.dart';
-import 'dart:developer' as developer;
+// import 'dart:developer' as developer;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  developer.log('App started', name: 'main');
+  // developer.log('App started', name: 'main');
   await Firebase.initializeApp();
   runApp(MyApp());
 }
