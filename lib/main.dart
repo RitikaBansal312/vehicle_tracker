1632fca8516402fca11ed5e7ebb5922348c2c711
ritikabansal312 <ritikabansal5678@gmail.com>
2025-01-03 13:28:18 +0530

    some basic changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:vehicle_tracker/views/vehicle_screen.dart';
-// import 'dart:developer' as developer;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // developer.log('App started', name: 'main');
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
