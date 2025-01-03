e29dfda3caca15712f25ccbc71e2b9dff2f1c9e9
ritikabansal312 <ritikabansal5678@gmail.com>
2025-01-03 14:27:19 +0530

    commented code removed

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,14 +1,3 @@
-// import 'package:firebase_database/firebase_database.dart';
-
-// class FirebaseService {
-//   final DatabaseReference _vehicleRef =
-//       FirebaseDatabase.instance.ref('Vehicles');
-
-//   Stream<DatabaseEvent> getVehiclesStream() {
-//     return _vehicleRef.onValue;
-//   }
-// }
-
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
--- lib/views/vehicle_screen.dart
+++ lib/views/vehicle_screen.dart
@@ -1,40 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import '../controllers/vehicle_controller.dart';
-
-// class VehicleMapPage extends StatelessWidget {
-//   final VehicleController vehicleController = Get.put(VehicleController());
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Real-time Vehicle Tracking')),
-//       body: Obx(() {
-//         return GoogleMap(
-//           initialCameraPosition: CameraPosition(
-//             target: LatLng(77.5700919, 77.5700919),
-//             // zoom: 5,
-//           ),
-//           myLocationEnabled: true,
-//           myLocationButtonEnabled: true,
-//           mapType: MapType.normal,
-//           markers: vehicleController.vehicles.values.map((vehicle) {
-//             return Marker(
-//               markerId: MarkerId(vehicle.vehicles.carOne.name),
-//               position: LatLng(vehicle.vehicles.carOne.coordinate.latitude,
-//                   vehicle.vehicles.carOne.coordinate.longitude),
-//               infoWindow: InfoWindow(title: vehicle.vehicles.carOne.name),
-//               icon: BitmapDescriptor.defaultMarker,
-//             );
-//           }).toSet(),
-//         );
-//       }),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -59,25 +22,27 @@ class VehicleMapPage extends StatelessWidget {
 
           return GoogleMap(
             initialCameraPosition: const CameraPosition(
-              target: LatLng(12.971598, 77.594566), // Default location
+              target: LatLng(28.699774, 77.138596), // Default location
               zoom: 12,
             ),
-            markers: {
-              Marker(
-                markerId: MarkerId('test_marker'),
-                position: LatLng(12.971598, 77.594566),
-                infoWindow: InfoWindow(title: 'Test Marker'),
-              ),
-              ...vehicleController.vehicles.entries.map((entry) {
-                final vehicle = entry.value;
-                return Marker(
-                  markerId: MarkerId(entry.key),
-                  position: LatLng(vehicle.latitude, vehicle.longitude),
-                  infoWindow: InfoWindow(title: vehicle.name),
-                  icon: BitmapDescriptor.defaultMarker,
-                );
-              }).toSet(),
-            },
+            markers:
+                // {
+                // Marker(
+                //   markerId: MarkerId('test_marker'),
+                //   position: LatLng(12.971598, 77.594566),
+                //   infoWindow: InfoWindow(title: 'Test Marker'),
+                // ),
+                // ...
+                vehicleController.vehicles.entries.map((entry) {
+              final vehicle = entry.value;
+              return Marker(
+                markerId: MarkerId(entry.key),
+                position: LatLng(vehicle.latitude, vehicle.longitude),
+                infoWindow: InfoWindow(title: vehicle.name),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+            }).toSet(),
+            // },
           );
         },
       ),
